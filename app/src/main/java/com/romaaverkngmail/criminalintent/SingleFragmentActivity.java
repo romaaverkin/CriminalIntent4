39de33487c07ce025d4d7fd0ec3446280c177732
romaaverkin@gmail.com <BeatRolMet1>
2018-05-14 11:01:30 +0700

    Переименовал activity_crime.xml в activity_fragment.xml, т. к. это предстовление универсально и может использоваться для любой активности. Создал абстрактный класс SingleFragmentActivity.java. Код этого класса будет использоваться почти в каждой активности.

--- app/src/main/java/com/romaaverkngmail/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/romaaverkngmail/criminalintent/SingleFragmentActivity.java
@@ -1,11 +1,13 @@
 package com.romaaverkngmail.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,7 @@ public class CrimeActivity extends AppCompatActivity {
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
