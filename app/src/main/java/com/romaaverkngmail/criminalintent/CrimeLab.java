33d0166f19f79fd61b169db03b95abc227ba7a59
Roman <romaaverkin@gmail.com>
2018-05-29 12:38:05 +0700

    Упражнение. Улучшение быстродействия CrimeLab

--- app/src/main/java/com/romaaverkngmail/criminalintent/CrimeLab.java
+++ app/src/main/java/com/romaaverkngmail/criminalintent/CrimeLab.java
@@ -3,14 +3,15 @@ package com.romaaverkngmail.criminalintent;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
-
-    private List<Crime> mCrimes;
+    private Map<UUID, Crime> mCrimesMap;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -20,26 +21,21 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
+        mCrimesMap = new LinkedHashMap<>();
         for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0); // Для каждого второго объекта
             crime.setRequiresPolice(i % 3 == 0); // Для каждого третьего объекта
-            mCrimes.add(crime);
+            mCrimesMap.put(crime.getId(), crime);
         }
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>(mCrimesMap.values());
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
-        return null;
+        return mCrimesMap.get(id);
     }
 }
